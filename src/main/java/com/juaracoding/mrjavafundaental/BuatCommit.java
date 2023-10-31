569d73ca07f6b1f2c34adcee21a04c9937d166f5
Raihan <c297dsx4862@bangkit.academy>
2023-10-31 09:50:28 +0700

    Adding 2 new files

---
+++ src/main/java/com/juaracoding/mrjavafundaental/BuatCommit.java
@@ -0,0 +1,13 @@
+package com.juaracoding.mrjavafundaental;
+/*
+IntelliJ IDEA 2023.2.4 (Community Edition)
+Build #IC-232.10203.10, built on October 25, 2023
+@Author Raihan a.k.a. Muhammad Raihan
+Java Developer
+Created on 10/31/2023 9:48 AM
+@Last Modified 10/31/2023 9:48 AM
+Version 1.0
+*/
+
+public class BuatCommit {
+}
